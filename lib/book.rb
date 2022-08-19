d88ab2556cf70c3c97c632089ea3a9cf270a8d82
Mwenje <mwenje24@gmail.com>
2022-08-19 06:46:53 +0300

    completed assignemnt

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+    attr_accessor :title, :author, :genre, :page_count
 
+    def initialize(title)
+        @title = title        
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+book = Book.new("ruby basics")
+book.author = "Dee Christie"
+book.genre = "Ruby"
+book.page_count = 272
+book.turn_page
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+class Shoe
+
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
+
+shoe = Shoe.new("Addidas")
+shoe.color = "white stripped"
+shoe.size = 8
+shoe.condition = "new"
